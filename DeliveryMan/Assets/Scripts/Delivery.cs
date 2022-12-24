d86df795dc1b36263a57833015dc988d57dab1a9
kim-hyunjin <hyunjin1612@gmail.com>
2022-12-24 16:54:36 +0900

    destory game object, GetComponent

---
+++ DeliveryMan/Assets/Scripts/Delivery.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Delivery : MonoBehaviour
+{
+    bool hasPackage;
+    [SerializeField] float delayToDestroy = 0.5f;
+    [SerializeField] Color32 hasPackageColor = new Color32(255, 0, 0, 255);
+    [SerializeField] Color32 noPackageColor = new Color32(255, 255, 255, 255);
+
+    SpriteRenderer spriteRenderer;
+
+    private void Start() {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D other) {
+        Debug.Log("아야!");
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.tag == "Package")
+        {
+            pickUpPackage(other);
+        }
+
+        if (other.tag == "Customer" && hasPackage)
+        {
+            deliverToCustomer();
+        }
+    }
+
+    private void deliverToCustomer() {
+        if (hasPackage) 
+        {
+            Debug.Log("delievered to customer");
+            hasPackage = false;
+            spriteRenderer.color = noPackageColor;
+        }
+    }
+
+    private void pickUpPackage(Collider2D other) {
+        if (hasPackage) return;
+
+        Debug.Log("Package picked up");
+        hasPackage = true;
+        spriteRenderer.color = hasPackageColor;
+        Destroy(other.gameObject, delayToDestroy);
+    }
+}
