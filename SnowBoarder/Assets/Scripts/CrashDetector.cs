e883ca48d47698c6fedd68d34de926fb0c0e4b03
kim-hyunjin <hyunjin1612@gmail.com>
2024-04-23 23:13:23 +0900

    사운드 이펙트

--- SnowBoarder/Assets/Scripts/CrashDetector.cs
+++ SnowBoarder/Assets/Scripts/CrashDetector.cs
@@ -7,10 +7,12 @@ public class CrashDetector : MonoBehaviour
 {
     [SerializeField] float loadDelay = 0.5f;
     [SerializeField] ParticleSystem crashEffect;
+    [SerializeField] AudioClip crashSFX;
     private void OnTriggerEnter2D(Collider2D other) {
         if (other.tag == "Ground")
         {
             crashEffect.Play();
+            GetComponent<AudioSource>().PlayOneShot(crashSFX);
             Invoke("ReloadScene", loadDelay);
         }
     }
